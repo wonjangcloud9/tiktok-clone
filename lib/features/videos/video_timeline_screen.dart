c52aedbf342a225fd04269c8a89d30b12c6adc58
wonjangcloud9 <venuscwj@naver.com>
2023-03-17 15:18:01 +0900

    infinite scrolling

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -8,6 +8,8 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
   List<Color> colors = [
     Colors.blue,
     Colors.red,
@@ -15,13 +17,36 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     Colors.teal,
   ];
 
+  void _onPageChange(int page) {
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll(
+        [
+          Colors.blue,
+          Colors.red,
+          Colors.yellow,
+          Colors.teal,
+        ],
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
       scrollDirection: Axis.vertical,
-      itemCount: 4,
+      onPageChanged: _onPageChange,
+      itemCount: _itemCount,
       itemBuilder: (context, index) => Container(
         color: colors[index],
+        child: Center(
+          child: Text(
+            "Screen $index",
+            style: const TextStyle(
+              fontSize: 68,
+            ),
+          ),
+        ),
       ),
     );
   }
