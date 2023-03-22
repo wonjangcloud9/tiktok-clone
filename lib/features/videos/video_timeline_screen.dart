7a659a69800459bd1e88fe8e7833d8961bf13ae9
wonjangcloud9 <venuscwj@naver.com>
2023-03-22 22:41:35 +0900

    PageController

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -10,6 +10,8 @@ class VideoTimelineScreen extends StatefulWidget {
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   int _itemCount = 4;
 
+  final PageController _pageController = PageController();
+
   List<Color> colors = [
     Colors.blue,
     Colors.red,
@@ -18,23 +20,29 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   ];
 
   void _onPageChange(int page) {
-    if (page == _itemCount - 1) {
-      _itemCount = _itemCount + 4;
-      colors.addAll(
-        [
-          Colors.blue,
-          Colors.red,
-          Colors.yellow,
-          Colors.teal,
-        ],
-      );
-      setState(() {});
-    }
+    _pageController.animateToPage(
+      page,
+      duration: const Duration(
+        milliseconds: 150,
+      ),
+      curve: Curves.linear,
+    );
+    _itemCount = _itemCount + 4;
+    colors.addAll(
+      [
+        Colors.blue,
+        Colors.red,
+        Colors.yellow,
+        Colors.teal,
+      ],
+    );
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
+      controller: _pageController,
       scrollDirection: Axis.vertical,
       onPageChanged: _onPageChange,
       itemCount: _itemCount,
