3c11669158f8e2b6424461e911f7f42ecd99d8b4
wonjangcloud9 <venuscwj@naver.com>
2023-03-25 14:02:07 +0900

    scrollBar & padding

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -14,6 +14,8 @@ class VideoComments extends StatefulWidget {
 class _VideoCommentsState extends State<VideoComments> {
   bool _isWriting = false;
 
+  final ScrollController _scrollController = ScrollController();
+
   void _onClosePressed() {
     Navigator.of(context).pop();
   }
@@ -61,63 +63,68 @@ class _VideoCommentsState extends State<VideoComments> {
           onTap: _onStopWriting,
           child: Stack(
             children: [
-              ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size16,
-                  vertical: Sizes.size10,
-                ),
-                separatorBuilder: (context, index) => Gaps.v20,
-                itemCount: 10,
-                itemBuilder: (context, index) => Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 18,
-                      child: Text("원장"),
-                    ),
-                    Gaps.h10,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+              Scrollbar(
+                controller: _scrollController,
+                child: ListView.separated(
+                  padding: const EdgeInsets.only(
+                    left: Sizes.size16,
+                    right: Sizes.size16,
+                    top: Sizes.size10,
+                    bottom: Sizes.size10 + Sizes.size72,
+                  ),
+                  separatorBuilder: (context, index) => Gaps.v20,
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 18,
+                        child: Text("원장"),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "test",
+                                  style: TextStyle(color: Colors.grey.shade500),
+                                ),
+                                Gaps.h10,
+                                Text(
+                                  "test",
+                                  style: TextStyle(color: Colors.grey.shade500),
+                                ),
+                              ],
+                            ),
+                            Gaps.v5,
+                            Text(
+                              "test",
+                              style: TextStyle(color: Colors.grey.shade500),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Gaps.h10,
+                      Column(
                         children: [
-                          Row(
-                            children: [
-                              Text(
-                                "test",
-                                style: TextStyle(color: Colors.grey.shade500),
-                              ),
-                              Gaps.h10,
-                              Text(
-                                "test",
-                                style: TextStyle(color: Colors.grey.shade500),
-                              ),
-                            ],
+                          FaIcon(
+                            FontAwesomeIcons.heart,
+                            size: Sizes.size20,
+                            color: Colors.grey.shade500,
                           ),
-                          Gaps.v5,
+                          Gaps.v2,
                           Text(
-                            "test",
-                            style: TextStyle(color: Colors.grey.shade500),
+                            "52.2K",
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    Gaps.h10,
-                    Column(
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size20,
-                          color: Colors.grey.shade500,
-                        ),
-                        Gaps.v2,
-                        Text(
-                          "52.2K",
-                          style: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               Positioned(
