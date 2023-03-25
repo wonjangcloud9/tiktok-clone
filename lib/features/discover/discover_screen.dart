cb55b6e67706fcc2f22c06af0854b67ed9c7ca3e
wonjangcloud9 <venuscwj@naver.com>
2023-03-25 15:52:17 +0900

    tabBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
+
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Discover"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text("Discover"),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontSize: Sizes.size16,
+              fontWeight: FontWeight.w600,
+            ),
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            indicatorColor: Colors.black,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(tab),
+              ),
+          ],
+        ),
       ),
     );
   }
