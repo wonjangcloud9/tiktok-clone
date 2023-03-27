fbb0096de5e51b26bbbecc63b5ea5569bf2cd5b8
wonjangcloud9 <venuscwj@naver.com>
2023-03-27 23:02:08 +0900

    splash off

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
+        splashColor: Colors.transparent,
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Color(0xFFE9435A),
         ),
