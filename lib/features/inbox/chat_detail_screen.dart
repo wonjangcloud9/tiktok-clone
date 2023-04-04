38fa0dbdd2dc16dc6abdae3ee52aa57be43b29e4
wonjangcloud9 <venuscwj@naver.com>
2023-04-04 23:55:32 +0900

    appBar active icon

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -12,6 +13,67 @@ class ChatDetailScreen extends StatefulWidget {
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size20,
+                backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/48057918?v=4',
+                ),
+              ),
+              Positioned(
+                right: -10,
+                bottom: -10,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: Sizes.size6,
+                    ),
+                  ),
+                  child: const CircleAvatar(
+                    radius: Sizes.size9,
+                    backgroundColor: Colors.lightGreen,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          title: const Text(
+            '원장',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: const Text(
+            '활동중',
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
