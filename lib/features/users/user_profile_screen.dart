dcaf17f28d0aafa5ce36ead69796efa116106bbf
wonjangcloud9 <venuscwj@naver.com>
2023-04-05 18:56:09 +0900

    SliverGrid

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -15,8 +16,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          snap: true,
-          floating: true,
+          pinned: true,
           stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
@@ -38,7 +38,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
               (context, index) => Container(
-                color: Colors.teal,
+                color: Colors.amber[100 * (index % 9)],
                 child: Align(
                   alignment: Alignment.center,
                   child: Text(
@@ -47,7 +47,27 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                 ),
               ),
             ),
-            itemExtent: 100)
+            itemExtent: 100),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Item $index",
+                ),
+              ),
+            ),
+          ),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1,
+          ),
+        )
       ],
     );
   }
