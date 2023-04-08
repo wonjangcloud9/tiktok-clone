891ab91688380587dac78efa9fe7de5514403b85
wonjangcloud9 <venuscwj@naver.com>
2023-04-08 10:44:21 +0900

    indicator

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -11,24 +12,9 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: ListWheelScrollView(
-        itemExtent: 100,
-        children: [
-          for (var x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                child: const Text(
-                  "pick me",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 39,
-                  ),
-                ),
-                color: Colors.teal,
-                alignment: Alignment.center,
-              ),
-            ),
+      body: Column(
+        children: const [
+          CircularProgressIndicator.adaptive(),
         ],
       ),
     );
