dec9fe26c5ce5c141f5f9631abd87d667269d9e7
wonjangcloud9 <venuscwj@naver.com>
2023-04-14 23:05:46 +0900

    selfieMode bug fix

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -19,7 +19,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
 
   late FlashMode _flashMode;
 
-  late final CameraController _cameraController;
+  late CameraController _cameraController;
 
   Future<void> initCamera() async {
     final cameras = await availableCameras();
@@ -58,6 +58,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
 
   Future<void> _toggleSelfieMode() async {
     _isSelfieMode = !_isSelfieMode;
+    print(_isSelfieMode);
     await initCamera();
     setState(() {});
   }
