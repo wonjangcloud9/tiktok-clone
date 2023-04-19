287e15f423c62530f57e81cb65502712e7e70064
wonjangcloud9 <venuscwj@naver.com>
2023-04-20 00:00:16 +0900

    noCamera

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -83,7 +83,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
   void dispose() {
     _buttonAnmationController.dispose();
     _progressAnimationController.dispose();
-    if (_noCamera) {
+    if (!_noCamera) {
       _cameraController.dispose();
     }
     super.dispose();
@@ -91,6 +91,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen>
 
   @override
   Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (_noCamera) return;
     if (!_hasPermission) return;
     if (!_cameraController.value.isInitialized) return;
     if (state == AppLifecycleState.inactive) {
